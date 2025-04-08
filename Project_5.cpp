ebd3ac53a5103004cafd41d648401467cf30b3ce
dpak <jdpak592@gmail.com>
2025-04-08 12:17:28 -0500

    New proj 5 code

--- Project_5.cpp
+++ Project_5.cpp
@@ -4,35 +4,26 @@
 // Date     : 04-03-2025                                                             *
 //************************************************************************************
 
-#include <Servo.h> // Include the Servo library
+#include <PWMServo.h> // Include the Servo library
 
 
 PWMServo fan; // create servo object to control the fan
- const int CENTRAL_FAN_PWM = ???; // Select correct pin for central fan
-
- // Setup function for central fan
- void fan_setup() {
-    fan.attach(CENTRAL_FAN_PWM); // attaches the fan to specified
-    // Arduino pin to the object
-    delay(100);
-    fan.write(20); // write low throttle
-    delay(3000);
- }
+ const int CENTRAL_FAN_PWM = 23; // Select correct pin for central fan
 
 // Defining pins for lateral motors & switch (select the correct pins)
-const int LeftMotor_PWM =  ???;
-const int LeftMotor_INA =  ???;
-const int LeftMotor_INB =  ???;
+const int LeftMotor_PWM =  3;
+const int LeftMotor_INA =  32;
+const int LeftMotor_INB =  31;
 
-const int RightMotor_PWM = ???;      
-const int RightMotor_INA = ???;      
-const int RightMotor_INB = ???;      
+const int RightMotor_PWM = 6;      
+const int RightMotor_INA = 30;      
+const int RightMotor_INB = 29;      
 
-const int RearMotor_PWM =  ???;
-const int RearMotor_INA =  ???;
-const int RearMotor_INB =  ???;
+const int RearMotor_PWM =  9;
+const int RearMotor_INA =  28;
+const int RearMotor_INB =  27;
 
-const int SWITCH_PIN = ???;
+const int SWITCH_PIN = 2;
 
 // Defining constants for hovercraft control (idle, power limits in each direction, and fsm interval)
 const int Idle = 0;
@@ -69,13 +60,108 @@ void setup () {
 
     // Configure the swith pin as an input with a pull-up resistor.
     pinMode(Switch_Pin, INPUT_PULLUP);
+
+    // Configure central fan pin as output
+    pinMode(CENTRAL_FAN_PWM, OUTPUT);
 }
+
+
+}
+
+float bound(float value, float min_value, float max_value) {
+
+  /*
+  Bound Function
+
+  Inputs: 
+  - value, min_value, max_value
+
+  Outputs:
+  - value, min_value, OR max_value
+
+  Description: 
+  This function receives input from the set_motor function to know the duty cycle each motor is operating at. The function then checks to make sure the input
+  value is within the min and max range, and if the value is outside of this range, the function returns the respective min or max value so that the set_motor 
+  function can correct itself to make sure the motors aren't running too fast. 
+  
+  */
+  
+    if (value < min_value) return min_value;
+    if (value > max_value) return max_value;
+    return value;
+}
+
+void set_motor(int pwmPin, int inAPin, int inBPin, float val) {
+
+  /*
+  set_motor function
+
+  Inputs: 
+  - pwmPin (int)
+  - inAPin (int)
+  - inBPin (int)
+  - val (float)
+
+  Outputs:
+  - none
+
+  Description:
+  This function controls the speed and direction of a motor by adjusting its PWM signal and direction pins. 
+  */
+
+    // Calling the bound function to correct for any outlying motor speed values that may be present.
+    val = bound(val, -Max_Val, Max_Val);
+
+    // if val > 0,make the fan run forward
+    if (val > 0) {
+        digitalWrite(inAPin, HIGH);
+        digitalWrite(inBPin, LOW);
+    } 
+
+    // if val < 0, make the fan run backward
+    else if (val < 0) {
+        digitalWrite(inAPin, LOW);
+        digitalWrite(inBPin, HIGH);
+    } 
+
+    // if val = 0, turn the fan off
+    else {
+        digitalWrite(inAPin, LOW);
+        digitalWrite(inBPin, LOW);
+    }
+
+    // send the absolute value of val to the pwmPin
+    analogWrite(pwmPin, abs(val));
+
+ // Setup function for central fan
+ void fan_setup() {
+    fan.attach(CENTRAL_FAN_PWM); // attaches the fan to specified
+    // Arduino pin to the object
+    delay(100);
+    fan.write(20); // write low throttle
+    delay(3000);
+ }
 
 void set_hovercraft_forces(float fx, float fy, float torque){
-//     // Set thrust levels for the hovercraft
+    // Set thrust levels for the hovercraft
+    int R = 5.5;
+ 
+    // using inverse kinematics matrix to define the motor speeds
+    motor_values[0] = (fx/(2*cos(30))) - (fy/(2+2*sin(30))) - (torque/(2*R*(1+sin(30))));
+    motor_values[1] = (fx/(2*cos(30))) + (fy/(2+2*sin(30))) + (torque/(2*R*(1+sin(30))));
+    motor_values[2] = (-fy/(1+sin(30))) + (torque * sin(30)/(R*(1 + sin(30))));
+    set_motors(motor_values);
 }
 
-
+void set_motors(float val[3]) {
+    const float negative_gain[3] = {0.8, 1.0, 1.0};
+    for (int i = 0; i < 3; i++) {
+        if (val[i] < 0) val[i] *= negative_gain[i];
+    }
+    set_motor(LeftMotor_PWM, LeftMotor_INA, LeftMotor_INB, val[0]);
+    set_motor(RightMotor_PWM, RightMotor_INA, RightMotor_INB, val[1]);
+    set_motor(RearMotor_PWM, RearMotor_INA, RearMotor_INB, val[2]);
+}
 
 typedef enum state{
     STATE_WAITING_FOR_SWITCH,
